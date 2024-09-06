c982333345b0301298d3de49c67b83978ba458c2
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-09-06 16:51:18 +0530

    getting profile

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -7,12 +7,19 @@ const authToken = (req, res, next) => {
   const token = req.headers["authorization"].split(" ")[1];
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET_KEY, (error, user) => {
-        req.user = user;
-next()
+      req.user = user;
+      next();
     });
   }
 };
 
-router.get("/", authToken);
+router.get("/", authToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.json(500).json({ error: "profile Fetching Error" });
+  }
+});
 
 module.exports = router;
