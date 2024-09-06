2902a3666a018f4cceaf17a55a4879a0c4651cb1
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-09-06 15:18:17 +0530

    Login Added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt =  require("bcrypt");
 const User = require('../models/User');
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/signup", async (req,res)=>{
@@ -15,6 +16,20 @@ try{
 res.status(500).json({error:error.message});
 }
 })
-
+router.post("/login",async(req,res)=>{
+const {username,password} = req.body;
+try{
+const user = await User.findOne({username:username});
+const authenticated = await bcrypt.compare(password,user.password);
+if(user && authenticated){
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:"1h"});
+    res.json({token:token});
+}else{
+    res.status(401).json({error:"Invalid credentials"});
+}
+}catch(error){
+res.staus(500).json({error:"Error while logging In "});
+}
+})
 
 module.exports = router;
