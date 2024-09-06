a1e2c94d8fd8b68f66c528439d9853f3f71d76eb
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-09-06 12:21:37 +0530

    testing route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/',(req,res)=>{
+res.send("Hello World");
+})
+
 app.listen(PORT,()=>{
     console.log("Server Running on...",`http://localhost:${PORT}`)
 })
