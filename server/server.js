77ed0fee514682099fb469a636f45d47661bf6c3
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-09-06 12:16:11 +0530

    server added

--- server/server.js
+++ server/server.js
@@ -2,5 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const dbConnection = require("./config/db");
 require("dotenv").config();
-
 dbConnection();
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT,()=>{
+    console.log("Server Running on...",`http://localhost:${PORT}`)
+})
